71abeed383e28c6553a4139ab75bb124f9f14056
zhongmc <zhongmc_me@163.com>
2017-07-05 19:22:43 +0800

    add swagger2

--- src/main/java/com/zmc/controller/HeroesController.java
+++ src/main/java/com/zmc/controller/HeroesController.java
@@ -6,6 +6,8 @@ import com.zmc.domain.Heroes;
 import com.zmc.domain.Result;
 import com.zmc.service.HeroesService;
 import com.zmc.utils.ResultUtil;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -17,13 +19,16 @@ import java.util.List;
  * Created by zhongmc on 2017/4/5.
  */
 @RestController
+@RequestMapping("/heroes")
 public class HeroesController {
     @Autowired
     private HeroesService heroesService;
     /*
      * 通过id获取heroes
      * */
-    @GetMapping("/heroes/{id}")
+    @ApiOperation(value = "获取英雄详情",notes = "根据ID获取英雄详情")
+    @ApiImplicitParam(name = "id",value = "英雄ID",required = true,dataType = "Integer")
+    @GetMapping("/{id}")
     public Result getHeroesById(@PathVariable("id") Integer id) throws Exception {
         Heroes heroes = heroesService.getOneHeroById(id);
         return ResultUtil.success(heroes,heroes==null?0:1);
@@ -32,7 +37,8 @@ public class HeroesController {
     /*
     * 获取英雄列表
     * */
-    @GetMapping("/heroes")
+    @ApiOperation(value = "获取英雄列表")
+    @GetMapping("/")
     public Result getAllHeroes() throws Exception {
         List<Heroes> heroes = heroesService.getAllHeroesAsList();
         return ResultUtil.success(heroes,heroes==null?0:heroes.size());
@@ -40,7 +46,9 @@ public class HeroesController {
     /*
     * 添加英雄
     * */
-    @PostMapping("/heroes")
+    @ApiOperation(value = "新增英雄")
+    @ApiImplicitParam(name = "heroes",value = "新增的英雄",required = true,dataType = "Heroes")
+    @PostMapping("/")
     public Result addHeroes(@Validated Heroes heroes, BindingResult bindingResult) throws Exception {
         if (bindingResult.hasErrors()){
             throw new HeroesException(ResultStatus.ARGUMENT_BINDING_ERROR);
@@ -52,7 +60,9 @@ public class HeroesController {
     /*
     * 删除英雄
     * */
-    @DeleteMapping("/heroes/{id}")
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除英雄",notes = "根据ID删除英雄")
+    @ApiImplicitParam(name = "id",value = "英雄ID",required = true,dataType = "Integer")
     @ResponseStatus
     public Result delHeroById(@PathVariable("id") Integer id) {
         try {
@@ -66,7 +76,9 @@ public class HeroesController {
     /*
     * 更新英雄
     * */
-    @PutMapping("/heroes/{id}")
+    @ApiOperation(value = "更新英雄",notes = "根据ID更新英雄")
+    @ApiImplicitParam(name = "id",value = "英雄ID",required = true,dataType = "Integer")
+    @PutMapping("/{id}")
     public Result updataHeroById(@PathVariable("id") Integer id,
                                  @Validated  Heroes heroes,BindingResult result) throws Exception {
         if (result.hasErrors()){
--- src/main/java/com/zmc/serviceImpl/HeroesServiceImpl.java
+++ src/main/java/com/zmc/serviceImpl/HeroesServiceImpl.java
@@ -6,6 +6,7 @@ import com.zmc.exception.HeroesException;
 import com.zmc.repository.HeroesRepository;
 import com.zmc.service.HeroesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +20,13 @@ public class HeroesServiceImpl implements HeroesService {
     @Autowired
     private HeroesRepository heroesRepository;
     @Override
-    @Cacheable(value = "hero",keyGenerator = "heroesKeyGenerator")
+    @Cacheable(value = "hero",key = "#id+''")
     public Heroes getOneHeroById(Integer id) throws Exception {
         System.out.println("from database");
         return heroesRepository.findOne(id);
     }
 
     @Override
-    @Cacheable(value = "heroese",keyGenerator = "heroesKeyGenerator")
     public List<Heroes> getAllHeroesAsList() throws Exception {
         System.out.println("from database");
         return heroesRepository.findAll();
@@ -43,6 +43,7 @@ public class HeroesServiceImpl implements HeroesService {
     }
 
     @Override
+    @CachePut(value = "hero",key = "#heroes.id+''")
     public Heroes updataOneHeroById(Heroes heroes) throws Exception {
         boolean isExisted = heroesRepository.exists(heroes.getId());
         if (isExisted){
